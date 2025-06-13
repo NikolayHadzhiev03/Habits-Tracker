94bd1589bdb8340ce35ee9f4cc88a5fff1c49c00
NikolayHadzhiev03 <nikolaiHadzhiev03@gmail.com>
2025-06-13 17:59:54 +0300

    Update Header.tsx

--- Habits/src/Components/HeaderandFooter/Header.tsx
+++ Habits/src/Components/HeaderandFooter/Header.tsx
@@ -9,10 +9,13 @@ export default function Header() {
       <div className="font-bold text-xl text-gray-800 dark:text-white">🌿 HabitTracker</div>
       <ul className="flex space-x-4 text-gray-700 dark:text-gray-300">
         <li><Link to="/" className="hover:text-blue-600">Home</Link></li>
-        <li><Link to="/login" className="hover:text-blue-600">Login</Link></li>
-        <li><Link to="/register" className="hover:text-blue-600">Register</Link></li>
-        <li><Link to="/profile" className="hover:text-blue-600">`{user?.username}`</Link></li>
-        <li><Link to="/add-habit" className="hover:text-blue-600">Add Habit</Link></li>
+        {user ? (
+          <><li><Link to="/profile" className="hover:text-blue-600">{user.username}</Link></li>
+            <li><Link to="/add-habit" className="hover:text-blue-600">Add Habit</Link></li></>
+        ) : (<>
+          <li><Link to="/login" className="hover:text-blue-600">Login</Link></li>
+          <li><Link to="/register" className="hover:text-blue-600">Register</Link></li>
+        </>)}
         <li><Link to="/stats" className="hover:text-blue-600">Stats</Link></li>
       </ul>
     </nav>
