48d8c6f98499d3497dd425d487aba3aed210f438
NikolayHadzhiev03 <nikolaiHadzhiev03@gmail.com>
2025-08-20 09:51:56 +0300

    Create a Store

---
+++ Habits/src/Store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+
+const store = configureStore({
+    reducer : {
+        //add a habit reducer
+    },
+});
+
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AddDispatch =typeof store.dispatch;
